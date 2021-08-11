59241598dbd45e2b61387f7786b4c39e431a896a
Aine Mota <motaaine1@gmail.com>
2021-08-11 14:11:18 -0300

    criei os arquivos para site SnapCrackle.

---
+++ snapcrackle.js
@@ -0,0 +1,35 @@
+let message = '';
+    let snap = document.querySelector('#snap');
+
+    maxValue = prompt('Escolha um número entre 10 e 500.');
+
+
+function snapCrackle(maxValue){
+
+    if(10 <= maxValue && maxValue<= 500){
+
+    for(let counter = 1; counter <= maxValue; counter++){
+        
+        if(counter % 5 === 0){
+            message += 'Crackle';
+
+        } 
+        if(!(counter % 2 === 0)) {
+            message += 'Snap';
+        }
+     
+        if(counter % 2 === 0 && !(counter % 5 === 0) ) {
+            message += counter;
+        }
+    message += ', ';    
+    }
+    return snap.innerHTML = message;
+
+    } else{
+    alert('O número está fora do intervalo solicitado.');
+    } 
+}
+snapCrackle(maxValue);
+
+
+
